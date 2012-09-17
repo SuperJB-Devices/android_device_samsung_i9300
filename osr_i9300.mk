6970d35d0b866af962079fb1de54f29e44c14b9c
superatmel <superatmel@gmail.com>
2012-09-17 08:22:26 +0200

    Prepare for OSR
    Change-Id: I2a16c260a6169f0651ad3b65ef5dafd544511921

--- osr_i9300.mk
+++ osr_i9300.mk
@@ -1,5 +1,5 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/osr/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := i9300
@@ -9,14 +9,14 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/osr/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9300/full_i9300.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9300
-PRODUCT_NAME := cm_i9300
+PRODUCT_NAME := osr_i9300
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9300
 PRODUCT_MANUFACTURER := samsung
